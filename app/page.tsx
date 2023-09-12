3f01d750a0a77ade6a9b340d7e633e2d520ecae0
ExerSolution <98365635+ExerSolution@users.noreply.github.com>
2023-09-12 21:15:21 +0800

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Landing() {
       <Navbar.Collapse>
           <Navbar.Link href="#" className="block mt-6 lg:text-3xl font-semibold pl-1 pr-4 text-white rounded md:bg-transparent md: text-base text-blue-700 md:p-0 md:dark:text-blue-500 " aria-current="page">Home</Navbar.Link>
           <Navbar.Link href="#about" className="block mt-6 lg:text-3xl font-semibold pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-base dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</Navbar.Link>
-          <img className="rounded-full w-20 h-20" src="./img/logo.png" alt="image description" />
+          {/* <img className="rounded-full w-20 h-20" src="./img/logo.png" alt="image description" /> */}
           <Navbar.Link href="#project" className="block mt-6 lg:text-3xl font-semibold pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-base dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Project</Navbar.Link>
           <Navbar.Link href="#contact" className="block mt-6 lg:text-3xl font-semibold pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 text-base dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</Navbar.Link>     
       </Navbar.Collapse>
@@ -33,7 +33,7 @@ export default function Landing() {
 <h2 className='text-base'>PROGRAMMER</h2>
 </div>
 <div className='mt-70'>
-  <p className='ml-5 md:text-2xl lg:text-5xl pt-20 pb-20'>I'm Yasser S. Magelna</p>
+  <p className='ml-5 md:text-2xl lg:text-5xl pt-20 pb-20'>I&apos;m Yasser S. Magelna</p>
   </div> 
   <div className='pb-20' style={{paddingBottom:'150px'}}> 
   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
@@ -41,7 +41,7 @@ export default function Landing() {
       About
   </span>
     <svg className="w-6 h-4 ml-2 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+    <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" d="M1 5h12m0 0L9 1m4 4L9 9"/>
   </svg>
 </button>
 </div>  
@@ -68,7 +68,7 @@ export default function Landing() {
             <h3 className="text-lg font-semibold text-white-900 dark:text-white">Brgy Santa Lucia St, Poblacion, Koronadal City, 9506 South Cotabato</h3>
             <time className="block mb-2 text-base font-normal leading-none text-purple-400 dark:text-purple-500">S.Y 2019-2023</time>
             <p className="text-xl font-normal text-blue-500 dark:text-blue-400">STI College Korondal</p>
-            <img className="a_img h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="./img/college1.png" alt="image description"/>
+            {/* <img className="a_img h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="./img/college1.png" alt="image description"/> */}
         </div>
     </li>
     
@@ -85,7 +85,7 @@ export default function Landing() {
             <h3 className="text-lg font-semibold text-white-900 dark:text-white">Brgy Santa Lucia St, Poblacion, Koronadal City, 9506 South Cotabato</h3>
             <time className="block mb-2 text-base font-normal leading-none text-purple-400 dark:text-purple-500">S.Y 2017-2019</time>
             <p className="text-xl font-normal text-blue-500 dark:text-blue-400">STI College Korondal</p>
-            <img className="a_img h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="./img/shs.jpg" alt="image description"/>
+            {/* <img className="a_img h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0" src="./img/shs.jpg" alt="image description"/> */}
         </div>
     </li>
     <li className="relative mb-6 sm:mb-0">
