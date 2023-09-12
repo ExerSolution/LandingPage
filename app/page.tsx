79caf991e4efe7fac712d0a74df86e8c8eb7444a
ExerSolution <98365635+ExerSolution@users.noreply.github.com>
2023-09-12 12:02:40 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Landing() {
 <h2 className='text-base'>PROGRAMMER</h2>
 </div>
 <div className='mt-70'>
-  <p className='ml-5 md:text-2xl lg:text-5xl pt-20 pb-20'>I'm Yasser S. Magelna</p>
+  <p className='ml-5 md:text-2xl lg:text-5xl pt-20 pb-20'>I&apos;m Yasser S. Magelna</p>
   </div> 
   <div className='pb-20' style={{paddingBottom:'150px'}}> 
   <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
